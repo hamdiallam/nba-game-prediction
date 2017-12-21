01762ebc6344e3a820908cbb1db7c10017c982ce
Hamdi Allam <hamdi.allam97@gmail.com>
2017-12-21 07:36:58 -0800

    persisting the model

--- dataset.py
+++ dataset.py
@@ -48,4 +48,10 @@ sess.run(train_step, feed_dict={x: x_train, y_: y_train})
 # Evaluating the model
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: x_train, y_: y_train}))
+print('Accuracy: ', sess.run(accuracy, feed_dict={x: x_train, y_: y_train}))
+
+
+# Save the model
+saver = tf.train.Saver()
+save_path = saver.save(sess, './bin/model.ckpt')
+print('Model saved in file: %s' % save_path)
