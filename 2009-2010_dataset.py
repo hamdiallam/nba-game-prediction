9895ee3829d29af1ff8361cbe4445e8f38d4edf1
Hamdi Allam <hamdi.allam97@gmail.com>
2017-12-24 01:46:54 -0800

    A little better model. Still alot of more to learn

--- 2009-2010_dataset.py
+++ 2009-2010_dataset.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
-import csv
-import os
+import csv, os
 
 pathname = './2009-2010.regular_season'
 
@@ -12,10 +11,15 @@ def load_season_data(pathname):
     filenames = os.listdir(pathname)
     for file in filenames:
         teams = file.split('.')[1]
-        reader = csv.reader(open(file))
-        # TODO: double check the append vs extend
-        train_x.extend(create_input_from_game(reader, teams[:3], teams[3:]))
-    return np.stack(train_x)
+        reader = csv.reader(open(pathname+'/'+file))
+        # some csv are corrupted
+        try:
+            x, y = create_input_from_game(reader, teams[:3], teams[3:])
+            train_x.extend(x); train_y.extend(y)
+        except:
+            print('FILE ERROR: ', file)
+        
+    return (np.stack(train_x), np.stack(train_y))
     
 
 def create_input_from_game(file, team1, team2):
@@ -51,11 +55,11 @@ def create_input_from_game(file, team1, team2):
         minute, second = int(current_time[0]), int(current_time[1])
         # delta of 15 seconds
         if minute == 6 and second > 15 and second <  45 and not any(x[4] == quarter_marker + 0.5 for x in input):
-            input.append([team1, team2, score1, score2, quarter_marker + 0.5])
+            input.append([hash(team1), hash(team2), score1, score2, quarter_marker + 0.5])
         # delta of 30 seconds for a play to occur within the beginning of a quarter
         #   - larger delta because the start of a quarter generally takes longer for a play to occur
         elif minute == 11 and second > 30 and not any(x[4] == quarter_marker for x in input): 
-            input.append([team1, team2, score1, score2, quarter_marker])
+            input.append([hash(team1), hash(team2), score1, score2, quarter_marker])
     
     # result of the game. Duplicate the same outcome for every play in the input matrix
     result = None
@@ -63,14 +67,78 @@ def create_input_from_game(file, team1, team2):
     for _ in range(len(input) - 1):
         result.append(result[0][:])
     
-    return (np.stack(input), np.stack(result))
+    return (input, result)
 
 
 # debuggin
 sample_file = os.listdir(pathname)[0]
-teams = sample_file.split('.')[1]
-team1, team2 = teams[:3], teams[3:]
+sample_teams = sample_file.split('.')[1]
+sample_team1, sample_team2 = sample_teams[:3], sample_teams[3:]
 
-reader = csv.reader(open(pathname+'/'+sample_file))
-result = create_input_from_game(reader, team1, team2)
+sample_reader = csv.reader(open(pathname+'/'+sample_file))
+sample_result = create_input_from_game(sample_reader, sample_team1, sample_team2)
 
+
+# train a NN model based on this data
+train_x, train_y = load_season_data(pathname)
+split_size = int(train_x.shape[0]*0.7) # 70/30 split between train/test
+
+train_x, val_x = train_x[:split_size], train_x[split_size:]
+train_y, val_y = train_y[:split_size], train_y[split_size:]
+
+# helper function
+def batch_creator(train_x, train_y, batch_size):
+    for i in range(0, train_x.shape[0], batch_size):
+        yield (train_x[i: i + batch_size], train_y[i: i + batch_size])
+
+
+# layers
+input_num_units = 5
+hidden_num_units = 3
+output_num_units = 2
+
+x = tf.placeholder(tf.float32, [None, input_num_units])
+y = tf.placeholder(tf.float32, [None, output_num_units])
+
+epochs = 6
+batch_size = 100
+learning_rate = 0.01
+
+seed = 128
+weights = {
+    'hidden': tf.Variable(tf.random_normal([input_num_units, hidden_num_units], seed=seed)),
+    'output': tf.Variable(tf.random_normal([hidden_num_units, output_num_units], seed=seed))
+}
+
+biases = {
+    'hidden': tf.Variable(tf.random_normal([hidden_num_units], seed=seed)),
+    'output': tf.Variable(tf.random_normal([output_num_units], seed=seed))
+}
+
+hidden_layer = tf.add(tf.matmul(x, weights['hidden']), biases['hidden'])
+hidden_layer = tf.nn.relu(hidden_layer)
+
+output_layer = tf.matmul(hidden_layer, weights['output']) + biases['output']
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output_layer, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+
+with tf.Session() as sess:
+    tf.global_variables_initializer().run()
+    for epoch in range(epochs):
+        avg_cost = 0
+        for batch_x, batch_y in batch_creator(train_x, train_y, batch_size):
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+            avg_cost += c / batch_size
+        print("Epoch:", (epoch+1), "cost =", "{:.5f}".format(avg_cost))
+
+    # find predictions on val set
+    pred_temp = tf.equal(tf.argmax(output_layer, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(pred_temp, tf.float32))
+    print("Validation Accuracy:", sess.run(accuracy, feed_dict={x: val_x, y: val_y}))
+
+    # Save the model
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, './bin2/model.ckpt')
+    print('Model saved in file: %s' % save_path)
