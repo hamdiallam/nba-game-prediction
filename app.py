f964b6c9f1252ade77c9c573b2878a12c5bf0674
Hamdi Allam <hamdi.allam97@gmail.com>
2017-12-24 02:11:01 -0800

    flask using the new model

--- app.py
+++ app.py
@@ -1,29 +1,57 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
 
-# variables used by the model
-W = tf.Variable(tf.zeros([2, 2]))
-b = tf.Variable(tf.zeros([1, 2]))
-x = tf.Variable(tf.zeros([1, 2]))
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+# layers
+input_num_units = 5
+hidden_num_units = 3
+output_num_units = 2
+
+x = tf.placeholder(tf.float32, [None, input_num_units])
+
+seed = 128
+weights = {
+    'hidden': tf.Variable(tf.random_normal([input_num_units, hidden_num_units], seed=seed), name='weights-hidden'),
+    'output': tf.Variable(tf.random_normal([hidden_num_units, output_num_units], seed=seed), name='weights-output')
+}
+
+biases = {
+    'hidden': tf.Variable(tf.random_normal([hidden_num_units], seed=seed), name='biases-hidden'),
+    'output': tf.Variable(tf.random_normal([output_num_units], seed=seed), name='biases-output')
+}
+
+hidden_layer = tf.add(tf.matmul(x, weights['hidden']), biases['hidden'])
+hidden_layer = tf.nn.relu(hidden_layer)
+
+output_layer = tf.matmul(hidden_layer, weights['output']) + biases['output']
+result = tf.nn.softmax(output_layer)
 
 # load in the graph
 sess = tf.Session()
-saver = tf.train.Saver({ 'W': W, 'b': b })
-saver.restore(sess, './bin/model.ckpt')
+saver = tf.train.Saver({ 'weights-hidden': weights['hidden'], 'weights-output': weights['output'],
+                        'biases-hidden': biases['hidden'], 'biases-output': biases['output'] })
+saver.restore(sess, './bin2/model.ckpt')
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def data():
+@app.route('/predict', methods=['GET'])
+def predict():
     try:
-        input = [[request.args.get('team1'), request.args.get('team2')]]
-        response = jsonify(sess.run(y, { x: input }).tolist()[0])
+        # default to midway through the game is period is not provided
+        period = None
+        if 'period' not in request.args:
+            period = 2
+        elif float(request.args.get('period')) not in [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]:
+            raise Exception('invalid period')
+
+        input = [[hash(request.args.get('home')), hash(request.args.get('away')),
+                  request.args.get('score1'), request.args.get('score2'), period or request.args.get('period')]]
+        response = jsonify(sess.run(result, { x: input }).tolist()[0])
         response.status_code = 200
         return response
     except Exception as e:
         response = jsonify({
-            'Message': 'Invalid query parameters. Use: ?team1=<score>&team2=<score>',
+            'Message': 'Invalid query parameters. Use: ?home=<team>&away=<team>&score1<int>&score2<int>&period=<1/1.5/2/2.5/3/3.5/4/4.5>',
+            'error': str(e)
         })
         response.status_code = 404
         return response
